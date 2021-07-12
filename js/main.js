7ab5d5666f1bc1173461e3f38ea79fedf7ef8ece
Joel Cuthriell <joel@cuthriell.com>
2021-07-12 00:20:59 -0500

    working refresh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 var app = new Vue({
     el: '#app',
-    data: {
-      message: 'Hello Vue!'
+    data () {
+      return {
+        info: null,
+        loading: true,
+        errored: false,
+      }
+    },
+    methods: {
+      getPot () {
+        this.polling = setInterval(() => {
+          axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => {
+            this.info = response.data.bpi
+          })
+          .catch(error => {
+            console.log(error)
+            this.errored = true
+          })
+          .finally(() => this.loading = false)
+        }, 50000)
+      }
+    },
+    filters: {
+      currencydecimal (value) {
+        return value.toFixed(2)
+      }
+    },
+    created () {
+      this.getPot();
     }
   })
