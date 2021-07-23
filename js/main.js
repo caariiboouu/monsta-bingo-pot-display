5e0d25d0bc3a5ae42c315cba8f331530dc6516f6
Joel Cuthriell <joel@cuthriell.com>
2021-07-23 00:05:30 -0500

    working and font applied

--- js/main.js
+++ js/main.js
@@ -9,19 +9,27 @@ var app = new Vue({
     },
     methods: {
       getPot () {
+        this.getPotSingleAction();
         this.polling = setInterval(() => {
-          axios
-          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-          .then(response => {
-            this.info = response.data.bpi
-          })
-          .catch(error => {
-            console.log(error)
-            this.errored = true
-          })
-          .finally(() => this.loading = false)
-        }, 50000)
-      }
+          this.getPotSingleAction();
+        }, 60000)
+      },
+      getPotRepeater () {
+        this.getPotSingleAction();
+      },
+      getPotSingleAction () {
+        axios
+        .get('https://cake.monster/api/bingo.php')
+        .then(response => {
+          this.info = response.data
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+      },
+
     },
     filters: {
       currencydecimal (value) {
